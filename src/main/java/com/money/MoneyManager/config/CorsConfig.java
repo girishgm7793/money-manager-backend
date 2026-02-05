69bcad31811ededdb20af8eca6e1d1dde603f549
girishgm7793 <girishgm@gmail.com>
2026-02-05 17:59:43 +0530

    fix cors for production

--- src/main/java/com/money/MoneyManager/config/CorsConfig.java
+++ src/main/java/com/money/MoneyManager/config/CorsConfig.java
@@ -7,18 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-	 @Bean
-	    public WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurer() {
-	            @Override
-	            public void addCorsMappings(CorsRegistry registry) {
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
 
-	                registry.addMapping("/**")
-	                        .allowedOrigins("*")   // IMPORTANT
-	                        .allowedMethods("*")
-	                        .allowedHeaders("*");
-	            }
-	        };
-	    }
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://money-manager-frontend.onrender.com"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
 
 }
