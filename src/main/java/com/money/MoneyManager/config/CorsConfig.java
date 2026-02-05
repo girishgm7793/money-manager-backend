d5f3657a9fa5eec0bbadfe71b33f60d259f17344
Girish Mhavale <ggmhavale2000@gmail.com>
2026-02-05 18:20:45 +0530

    Rename CorsConfig to WebConfig and modify CORS settings
    Renamed CorsConfig to WebConfig and updated CORS settings.

--- src/main/java/com/money/MoneyManager/config/CorsConfig.java
+++ src/main/java/com/money/MoneyManager/config/CorsConfig.java
@@ -6,23 +6,20 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class WebConfig {
+
 	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
 
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "https://money-manager-frontend.onrender.com"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(false);
-            }
-        };
-    }
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
 
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:5173", "https://money-manager-frontend-txkv.onrender.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
 }
